87210af1cf977308585969939201b5d9ae104f03
aowuyuz <1683434180@qq.com>
2024-05-13 20:08:05 +0800

    Delete flash_sale-master directory

--- flash_sale-master/shop-parent/shop-common/src/main/java/cn/wolfcode/common/constants/CommonConstants.java
+++
@@ -1,9 +0,0 @@
-package cn.wolfcode.common.constants;
-
-/**
- * Created by lanxw
- */
-public class CommonConstants {
-    public static final String TOKEN_NAME = "token";
-    public static final String REAL_IP = "X-REAL-IP";
-}
